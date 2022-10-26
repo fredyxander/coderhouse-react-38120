fac404c2be324f148dbb11b3add793ad7c34c3e2
fredyxander <freddy5210@gmail.com>
2022-10-26 12:03:17 -0500

    after firebase

--- Clases/Clase13/firebase2/src/components/Desafios/ItemDetailContainer/ItemDetailContainer.js
+++ Clases/Clase13/firebase2/src/components/Desafios/ItemDetailContainer/ItemDetailContainer.js
@@ -3,25 +3,39 @@ import './ItemDetailContainer.css';
 import { arregloProductos } from "../../baseDatos/baseDatos";
 import { ItemDetail } from "../ItemDetail/ItemDetail";
 import {useParams} from "react-router-dom";
+import {db} from "../../../utils/firebase";
+import {doc,getDoc} from "firebase/firestore";
 
 // /item/id
 export const ItemDetailContainer = ()=>{
     const {id} = useParams(); //{id:"8"}
     const [itemProduct, setItemProduct] = useState({});
 
-    const promesa = new Promise((resolve, reject)=>{
-        setTimeout(() => {
-            resolve(arregloProductos);
-        }, 2000);
-    })
+    // const promesa = new Promise((resolve, reject)=>{
+    //     setTimeout(() => {
+    //         resolve(arregloProductos);
+    //     }, 2000);
+    // })
 
     useEffect(()=>{
         const getProducto = async()=>{
-            const productos = await promesa;
-            // console.log('productos', productos);
-            const producto = productos.find(elemento=>elemento.id === parseInt(id));
-            // console.log("producto", producto)
-            setItemProduct(producto);
+            // const productos = await promesa;
+            // // console.log('productos', productos);
+            // const producto = productos.find(elemento=>elemento.id === parseInt(id));
+            // // console.log("producto", producto)
+            // setItemProduct(producto);
+
+            //creamos la referencia del documento que vamos a consultar
+            const queryRef = doc(db,"items",id);
+            //hacemos la consulta
+            const response = await getDoc(queryRef);
+            console.log(response)
+            const newDoc = {
+                ...response.data(),
+                id:response.id
+            }
+            // console.log(newDoc)
+            setItemProduct(newDoc)
         }
         getProducto();
     },[id])
--- Clases/Clase13/firebase2/src/components/Desafios/ItemListContainer/ItemListContainer.js
+++ Clases/Clase13/firebase2/src/components/Desafios/ItemListContainer/ItemListContainer.js
@@ -4,38 +4,59 @@ import './ItemListContainer.css';
 import { arregloProductos } from "../../baseDatos/baseDatos";
 import { ItemList } from "../ItemList/ItemList";
 import { useParams } from "react-router-dom";
+import {db} from "../../../utils/firebase";
+import { collection, getDocs, query, where } from "firebase/firestore";
 
 export const ItemListContainer = ()=>{
     console.log(useParams()); // {categoryName:""}
     // const categoryName = useParams().categoryName;
     const {categoryId} = useParams();
-    console.log(categoryId);
+    // console.log(categoryId);
     const [productos, setProductos] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-    const promesa = new Promise((resolve, reject)=>{
-        setTimeout(() => {
-            resolve(arregloProductos);
-        }, 2000);
-    })
+    // const promesa = new Promise((resolve, reject)=>{
+    //     setTimeout(() => {
+    //         resolve(arregloProductos);
+    //     }, 2000);
+    // })
 
     useEffect(()=>{
-        promesa.then((response)=>{
-            if(categoryId){
-                //vamos a filtrar por categoria
-                const productsFiltered = response.filter(elm=>elm.categoria === categoryId);
-                setProductos(productsFiltered);
-            } else{
-                //mostrar todos los productos
-                setProductos(response)
-            }
-        })
+        // promesa.then((response)=>{
+        //     if(categoryId){
+        //         //vamos a filtrar por categoria
+        //         const productsFiltered = response.filter(elm=>elm.categoria === categoryId);
+        //         setProductos(productsFiltered);
+        //     } else{
+        //         //mostrar todos los productos
+        //         setProductos(response)
+        //     }
+        // })
+        const queryRef = categoryId ? query(collection(db,"items"), where("category","==",categoryId)) : collection(db,"items");
+        getDocs(queryRef).then((response)=>{
+            const results = response.docs;
+            const docs = results.map(doc=>{
+                return{
+                    ...doc.data(),
+                    id:doc.id
+                }
+            });
+            // console.log(docs);
+            setProductos(docs);
+            setLoading(false);
+        });
     },[categoryId])
 
     // console.log("productos", productos);
     return(
         <div className="item-list-container">
             <p>item list container</p>
-            <ItemList items={productos} otraPropiedad={123}/>
+            {
+                loading ?
+                <p>cargando</p>
+                :
+                <ItemList items={productos} otraPropiedad={123}/>
+            }
         </div>
     )
 }
